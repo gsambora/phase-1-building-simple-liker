461627a07a7151c6bf642ded81bf68811c953887
Hana Sambora <grace.sambora@gmail.com>
2023-06-12 20:43:51 -0500

    Complete lab

--- main.js
+++ main.js
@@ -4,12 +4,47 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const errorModal = document.getElementById("modal")
+errorModal.classList.add("hidden")
+//console.log(errorModal.querySelector("p"))
+
+const input = document.querySelectorAll(".like-glyph")
+input.forEach(function(element){
+  //console.log(element)
+  //console.log(typeof(element))
+  element.addEventListener('click', function() {
+    //Change empty heart to full heart
+    if(element.innerHTML === EMPTY_HEART){
+      mimicServerCall()
+      .then(element.innerHTML = FULL_HEART)
+      .then(element.classList.add("activated-heart"))
+      .catch(function(error){
+        errorModal.classList.remove("hidden")
+        errorModal.querySelector("p").innerHTML = error
+      })
+    } 
+    //Change full heart to empty heart
+    else {
+      mimicServerCall()
+      .then(element.innerHTML = EMPTY_HEART)
+      .then(element.classList.remove("activated-heart"))
+      .catch(function(error){
+        errorModal.classList.remove("hidden")
+        errorModal.querySelector("p").innerHTML = error
+      })
+    }
+    
+
+  });
+  
+})
+
 
 
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
-//------------------------------------------------------------------------------
+//------err------------------------------------------------------------------------
 
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
   return new Promise(function(resolve, reject) {
